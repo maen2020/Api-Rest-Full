5dbf14519c29d03896cfce02e151cef3987f838a
maen <maen210212@gmail.com>
2023-05-01 15:54:26 -0600

    Project start and database configuration.

---
+++ src/main/java/com/maen/app/ApiRestFullApplication.java
@@ -0,0 +1,12 @@
+package com.maen.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestFullApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestFullApplication.class, args);
+	}
+}
---
+++ src/test/java/com/maen/app/ApiRestFullApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maen.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestFullApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
